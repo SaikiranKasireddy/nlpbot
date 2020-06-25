7573ba936a17b5e6d6c9efcea743cf951405e2cb
Snipta Mallick <mallicksnip@gmail.com>
2020-06-24 19:34:04 -0500

    env and django install

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/sniptamallick/Documents/GitHub/sniptamallick.github.io/utdchatbot/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
