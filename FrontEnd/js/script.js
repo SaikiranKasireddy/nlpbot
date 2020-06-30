4bb85ab877b882043f620af91c633fe9e7588d5d
Snipta Mallick <39253021+sniptamallick@users.noreply.github.com>
2020-06-29 20:53:23 -0500

    Basic Frontend
    Will be synced with Django later

---
+++ FrontEnd/js/script.js
@@ -0,0 +1,14 @@
+var chatButton = document.getElementById("chat");
+
+chatButton.addEventListener('click', function()
+{
+    window.open('../chatpage.html');
+});
+
+
+var returnhome = document.getElementById("back");
+
+returnhome.addEventListener('click', function()
+{
+    window.open('../index.html');
+});
